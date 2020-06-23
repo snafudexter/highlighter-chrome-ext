dff90cb6a9fba138b4dcbca78d6628214be780ba
prabhjot <prabh@live.in>
2020-06-24 00:01:46 +0530

    minor imporvements

--- content.js
+++ content.js
@@ -38,49 +38,59 @@ function init() {
                         popup.style.display = "none"
                     });
                 }
+                console.log('high', highlighting)
+                chrome.storage.local.get([URL], function (result) {
+                    if (!isEmpty(result)) {
+                        arr = result[URL]
+                        const len = arr.length;
+                        var found = window.find(arr.pop(), false, false, true)
+                        if (!found || len <= 0) {
+                            highlighting = false
+                        }
+                    }
+                });
             }
         }
     };
     xhr.open("GET", path_, true);
     xhr.send(null);
-    chrome.storage.local.get([URL], function (result) {
-        if (!isEmpty(result)) {
-            arr = result[URL]
-            var found = window.find(arr.pop(), false, false, true)
-            if (!found || arr.length <= 0) highlighting = false
-        }
-    });
 
 };
 
 document.onselectionchange = () => {
+    try {
+        if (window.getSelection().rangeCount > 0) {
+            var selection = window.getSelection().getRangeAt(0).cloneRange();
+            var rect = selection.getClientRects();
+            if (highlighting) {
+                highlightSelection()
+                if (arr.length > 0) {
+                    var found = false;
+                    do {
+                        found = window.find(arr.pop(), false, false, true)
+                    } while (!found && arr.length > 0)
 
-    var selection = window.getSelection().getRangeAt(0).cloneRange();
-    var rect = selection.getClientRects();
-    if (highlighting) {
-        highlightSelection()
-        if (arr.length > 0) {
-            var found = false;
-            do {
-                found = window.find(arr.pop(), false, false, true)
-            } while (!found && arr.length > 0)
+                } else {
+                    highlighting = false;
+                    window.getSelection().empty()
+                }
 
-        } else {
-            highlighting = false;
-            window.getSelection().empty()
+            } else {
+                if (rect.item(0) != null) {
+                    popup.style.cssText = `position:fixed; top: ${rect.item(0).top - 50}px; left: ${rect.item(0).left}px;`
+                    popup.style.display = "block"
+                }
+
+                if (selection.toString().length <= 0) {
+                    popup.style.display = "none"
+                }
+            }
         }
 
-    } else {
-        if (rect.item(0) != null) {
-            popup.style.cssText = `position:fixed; top: ${rect.item(0).top - 50}px; left: ${rect.item(0).left}px;`
-            popup.style.display = "block"
-        }
-
-        if (selection.toString().length <= 0) {
-            popup.style.display = "none"
-        }
+    } catch (err) {
+        console.log(window.getSelection().rangeCount)
+        console.log('err', err)
     }
-
 }
 
 function highlightSelection() {
